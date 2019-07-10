38bdd63e5da15fa64bb0da6e680c1918e41c6bbe
xin liu <xinliu253@yahoo.com>
2019-07-10 13:47:50 -0400

    Navigation bar changed for auth state

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,9 +1,3 @@
-import React from 'react';
+import AuthUserContext from './context';
 
-const Session = () => (
-  <div>
-    <h1>Session</h1>
-  </div>
-);
-
-export default Session;
+export { AuthUserContext };
